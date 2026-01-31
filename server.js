7470fb759c5bc08fb20f8903982ae19268e46fdf
taylan030 <taylan.orlando@gmail.com>
2026-01-31 10:13:11 +0100

    Change default port to 8080

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // ============================================
 // MIDDLEWARE
